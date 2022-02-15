6dfdae091bc6dcaa2a520b2ca13075e7cf0737dd
Mostafa Safwat <36309847+010101mostafa@users.noreply.github.com>
2022-02-15 06:41:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,23 @@
 function compute()
 {
     a =parseInt( document.getElementById("Amount").value);
-    if (a){
-        Rate=document.getElementById("Rate").value;
-        years=parseInt(document.getElementById("years").value);
-        result= a*Rate*years/100;
-        inyear=(new Date()).getFullYear()+years;
-        document.getElementById("result").innerHTML="<br>If you deposit <mark>"+a+"</mark>,<br>"+
-            "at an interest rate of <mark>"+Rate+"\%</mark><br>"+
-            "You will receive an amount of <mark>"+result+"</mark>,<br>"+
-            "in the year <mark>"+inyear+"</mark>";
-    }else
-    {
-        alert("enter a positive number !")
+    if (!a){
+        alert("enter a positive number !");
+        return;
     }
+    if (a<=0){
+        alert("enter a positive number !");
+        return;
+    }
+    Rate=document.getElementById("Rate").value;
+    years=parseInt(document.getElementById("years").value);
+    result= a*Rate*years/100;
+    inyear=(new Date()).getFullYear()+years;
+    document.getElementById("result").innerHTML="<br>If you deposit <mark>"+a+"</mark>,<br>"+
+        "at an interest rate of <mark>"+Rate+"\%</mark><br>"+
+        "You will receive an amount of <mark>"+result+"</mark>,<br>"+
+        "in the year <mark>"+inyear+"</mark>";
+
+
      
 }  
